e7a13b177ee3b4871cd320f329980f91e00e32e0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-25 19:33:51 +0100

    chore: updated API paths in axios configuration

--- src/app/axios/path.js
+++ src/app/axios/path.js
@@ -1,22 +1,25 @@
 import axios from "axios";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 
 const path = axios.create({
-  baseURL:  process.env.NEXT_PUBLIC_URL_API,
+  baseURL: process.env.NEXT_PUBLIC_URL_API,
 });
 
 path.interceptors.request.use(
   function (config) {
-    config.headers["Content-Type"] = "application/json";
-    const token = Cookies.get('token');
-    console.log('Token retrieved in interceptor:', token);
+    console.log("Axios Request Config:", config);
 
+    config.headers["Content-Type"] = "application/json";
+
+    const token = Cookies.get("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
-    } 
+    }
+
     return config;
   },
   function (err) {
+    console.error("Axios Request Error:", err);
     return Promise.reject(err);
   }
 );
